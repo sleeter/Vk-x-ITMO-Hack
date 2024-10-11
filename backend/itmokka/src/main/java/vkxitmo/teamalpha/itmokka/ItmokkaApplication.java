e08228dfb6f73770081daf0d99e3e826a438b15a
sleeter <lfmonsterfeed@gmail.com>
2024-10-12 01:01:58 +0300

    init project

---
+++ backend/itmokka/src/main/java/vkxitmo/teamalpha/itmokka/ItmokkaApplication.java
@@ -0,0 +1,13 @@
+package vkxitmo.teamalpha.itmokka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItmokkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItmokkaApplication.class, args);
+	}
+
+}
---
+++ backend/itmokka/src/test/java/vkxitmo/teamalpha/itmokka/ItmokkaApplicationTests.java
@@ -0,0 +1,13 @@
+package vkxitmo.teamalpha.itmokka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItmokkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
