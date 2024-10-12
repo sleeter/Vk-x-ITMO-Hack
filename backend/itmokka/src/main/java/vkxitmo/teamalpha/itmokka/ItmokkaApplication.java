ad5864a17688668711cb446653c52abd34e56bcd
sleeter <lfmonsterfeed@gmail.com>
2024-10-12 09:12:59 +0300

    add component scan

--- backend/itmokka/src/main/java/vkxitmo/teamalpha/itmokka/ItmokkaApplication.java
+++ backend/itmokka/src/main/java/vkxitmo/teamalpha/itmokka/ItmokkaApplication.java
@@ -2,8 +2,10 @@ package vkxitmo.teamalpha.itmokka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"vkxitmo.teamalpha.itmokka.configuration"})
 public class ItmokkaApplication {
 
 	public static void main(String[] args) {
