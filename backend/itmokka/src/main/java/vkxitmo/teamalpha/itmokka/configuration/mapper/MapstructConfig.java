adfcaf3b1db5627c13730476d83b3439f5cd6960
sleeter <lfmonsterfeed@gmail.com>
2024-10-12 23:04:24 +0300

    fix

--- backend/itmokka/src/main/java/vkxitmo/teamalpha/itmokka/configuration/mapper/MapstructConfig.java
+++ backend/itmokka/src/main/java/vkxitmo/teamalpha/itmokka/configuration/mapper/MapstructConfig.java
@@ -3,11 +3,13 @@ package vkxitmo.teamalpha.itmokka.configuration.mapper;
 import org.mapstruct.InjectionStrategy;
 import org.mapstruct.MapperConfig;
 import org.mapstruct.ReportingPolicy;
+import org.springframework.context.annotation.Configuration;
 
 @MapperConfig(
         componentModel = "spring",
         unmappedTargetPolicy = ReportingPolicy.ERROR,
         injectionStrategy = InjectionStrategy.CONSTRUCTOR)
+@Configuration
 public class MapstructConfig {
 
 }
