75ebeb5272e8818ece2932bb8dc1bf4598ea3df9
Alohhhis <alyona@niuitmo.ru>
2024-10-13 14:34:40 +0300

    Fixes

--- frontend/src/panels/BeFriendly.js
+++ frontend/src/panels/BeFriendly.js
@@ -9,7 +9,7 @@ import { Footer } from '../components/Footer.js';
 
 
 // URL API и пример ID пользователя
-const API_URL = 'http://84.201.137.6:8080/api/v1/topic/info';
+const API_URL = 'https://sleeter.ru/api/v1/topic/info';
 const PAGE_ID = 3;
 
 export const BeFriendly = ({ id }) => {
@@ -86,7 +86,7 @@ export const BeFriendly = ({ id }) => {
 
     // Рендеринг списка задач на основе данных из API
     const renderTasks = (taskList, handleCardClick) => (
-        <Div style={{ display: 'flex', flexDirection: 'column' }}>
+        <>
             {taskList.map((task) => {
                 let picturePath;
                 if (task.picture === 'place') {
@@ -111,7 +111,7 @@ export const BeFriendly = ({ id }) => {
                         }}
                     />
             )})}
-        </Div>
+            </>
     );
 
     return (
@@ -144,8 +144,12 @@ export const BeFriendly = ({ id }) => {
                                     {/* Категория Тесты */}
                                     {categorizedTasks.tests.length > 0 && (
                                         <Group>
+                                            <HorizontalScroll>
+                                                <Div style={{ display: 'flex', gap: '10px' }}>
                                             <Title level="2" weight="bold">Тесты</Title>
                                             {renderTasks(categorizedTasks.tests, () => routeNavigator.push(`/task?id=${1}`))}
+                                                </Div>
+                                            </HorizontalScroll>
                                         </Group>
                                     )}
 
@@ -155,9 +159,9 @@ export const BeFriendly = ({ id }) => {
                                             <Group>
                                                 <Title level="2" weight="bold">Мероприятия</Title>
                                                 <HorizontalScroll>
-                                                    <div style={{ display: 'flex', gap: '10px' }}>
+                                                    <Div style={{ display: 'flex', gap: '10px' }}>
                                                         {renderTasks(categorizedTasks.events, () => routeNavigator.push(`/task?id=${2}`))}
-                                                    </div>
+                                                    </Div>
                                                 </HorizontalScroll>
                                             </Group>
 
@@ -167,7 +171,11 @@ export const BeFriendly = ({ id }) => {
                                     {categorizedTasks.places.length > 0 && (
                                         <Group>
                                             <Title level="2" weight="bold">Места</Title>
+                                            <HorizontalScroll>
+                                                <Div style={{ display: 'flex', gap: '10px' }}>
                                             {renderTasks(categorizedTasks.places, handleCardClickForPlaces)}
+                                                </Div>
+                                            </HorizontalScroll>
                                         </Group>
                                     )}
                                 </>
