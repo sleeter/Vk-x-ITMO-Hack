a11c24d5b56f407966a3c001138b381abf502ec2
sleeter <lfmonsterfeed@gmail.com>
2024-10-13 14:35:17 +0300

    fix

--- backend/itmokka/src/main/java/vkxitmo/teamalpha/itmokka/dto/response/TopicOnlyResponse.java
+++ backend/itmokka/src/main/java/vkxitmo/teamalpha/itmokka/dto/response/TopicOnlyResponse.java
@@ -2,5 +2,5 @@ package vkxitmo.teamalpha.itmokka.dto.response;
 
 import java.util.List;
 
-public record TopicOnlyResponse(String name, String description, List<TaskResponse> taskList) {
+public record TopicOnlyResponse(String name, List<TaskResponse> taskList) {
 }
