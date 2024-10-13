98faad40eea581afeee7b3638ed03ebcd4a02e14
Alohhhis <alyona@niuitmo.ru>
2024-10-13 13:31:42 +0300

    Selection design

--- frontend/src/panels/Selection.js
+++ frontend/src/panels/Selection.js
@@ -1,4 +1,4 @@
-import {CardGrid, ContentCard, Panel} from '@vkontakte/vkui';
+import {CardGrid, ContentCard, Group, Panel} from '@vkontakte/vkui';
 import beITMO from '../assets/beITMO.jpg';
 import back from '../assets/back.svg';
 import PropTypes from 'prop-types';
@@ -6,77 +6,108 @@ import {Header} from '../components/Header.js';
 import {Footer} from "../components/Footer.js";
 import {useRouteNavigator} from '@vkontakte/vk-mini-apps-router';
 
-export const Selection = ({ id }) => {
+export const Selection = ({id}) => {
     const routeNavigator = useRouteNavigator();
 
     return (
-        <Panel id={id} style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
-            <Header auth={true} isLight={false} />
-
-            <CardGrid size="l" style={{
-                marginTop: '70px',
-                backgroundImage: `url(${back})`,
-                backgroundPosition: 'center',
-                flexGrow: 1,  // Это позволяет растянуть контейнер до максимальной высоты
+        <Panel id={id} style={{display: 'flex', flexDirection: 'column', minHeight: '100vh'}}>
+            <Header auth={true} isLight={false}/>
+            <Group style={{
+                margin: 0,
+                marginBottom: '20px',
+                padding: 0,
                 display: 'flex',
-                flexDirection: 'column',
                 justifyContent: 'center',
                 alignItems: 'center',
+                flexDirection: 'column',
+                backgroundImage: `url(${back})`,
+                height: '100%',
+                paddingTop: '40px',
             }}>
-                <ContentCard
-                    onClick={() => {
-                        routeNavigator.push('/be-itmo')
-                    }}
-                    src={beITMO}
-                    alt="Be ITMO"
-                    header="Be ITMO"
-                    text="Стиль жизни Университета ИТМО, основанный на принципах благополучия человека (well-being) и всестороннего развития членов ITMO Family"
-                    hasHover={true}
-                    style={{
-                        width: '30%',
-                        height: '33vw',
-                        margin: '20px auto',
-                        borderRadius: '10px',
-                        overflow: 'hidden',
-                    }}
-                />
-                <ContentCard
-                    onClick={() => {
-                        routeNavigator.push('/not-ready')
-                    }}
-                    src="https://i.pinimg.com/564x/ef/78/9d/ef789dddba116328ff84de7fc596c0eb.jpg"
-                    alt="Well-being challenge"
-                    header="Well-being challenge"
-                    text="Создание благоприятной среды для всестороннего развития членов ITMO.Family направлено на достижение баланса эмоционального, интеллектуального, физического и творческого состояний"
-                    hasHover={true}
-                    style={{
-                        width: '30%',
-                        height: '33vw',
-                        margin: '20px auto',
-                        borderRadius: '10px',
-                        overflow: 'hidden',
-                    }}
-                />
-                <ContentCard
-                    onClick={() => {
-                        routeNavigator.push('/not-ready')
-                    }}
-                    src="https://i.pinimg.com/564x/ea/f2/93/eaf293e294e72c0387d06514dcaeb18c.jpg"
-                    alt="Work-life balance"
-                    header="Work-life balance"
-                    text="Работай чтобы жить, а не живи, чтобы работать. Развиваемся в разных направлениях вместе с университетом"
-                    hasHover={true}
-                    style={{
-                        width: '30%',
-                        height: '33vw',
-                        margin: '20px auto',
-                        borderRadius: '10px',
-                        overflow: 'hidden',
-                    }}
-                />
-            </CardGrid>
+                <CardGrid size="l" style={{
+                    marginTop: '70px',
+                    backgroundPosition: 'center',
+                    flexGrow: 1,  // Это позволяет растянуть контейнер до максимальной высоты
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    width: '100%',
 
-            <Footer style={{ marginTop: 'auto' }} />
+                }}>
+                    <ContentCard
+                        onClick={() => {
+                            routeNavigator.push('/be-itmo')
+                        }}
+                        src={beITMO}
+                        alt="Be ITMO"
+                        header="Be ITMO"
+                        text="Стиль жизни Университета ИТМО, основанный на принципах благополучия человека (well-being) и всестороннего развития членов ITMO Family"
+                        hasHover={true}
+                        style={{
+                            width: '30%',         // Ширина для больших экранов
+                            height: '33vw',        // Адаптивная высота
+                            margin: '20px auto',
+                            borderRadius: '10px',
+                            overflow: 'hidden',
+                        }}
+                    />
+                    <ContentCard
+                        onClick={() => {
+                            routeNavigator.push('/not-ready')
+                        }}
+                        src="https://i.pinimg.com/564x/ef/78/9d/ef789dddba116328ff84de7fc596c0eb.jpg"
+                        alt="Well-being challenge"
+                        header="Well-being challenge"
+                        text="Создание благоприятной среды для всестороннего развития членов ITMO.Family направлено на достижение баланса эмоционального, интеллектуального, физического и творческого состояний"
+                        hasHover={true}
+                        style={{
+                            width: '30%',         // Ширина для больших экранов
+                            height: '33vw',        // Адаптивная высота
+                            margin: '20px auto',
+                            borderRadius: '10px',
+                            overflow: 'hidden',
+                        }}
+                    />
+                    <ContentCard
+                        onClick={() => {
+                            routeNavigator.push('/not-ready')
+                        }}
+                        src="https://i.pinimg.com/564x/ea/f2/93/eaf293e294e72c0387d06514dcaeb18c.jpg"
+                        alt="Work-life balance"
+                        header="Work-life balance"
+                        text="Работай чтобы жить, а не живи, чтобы работать. Развиваемся в разных направлениях вместе с университетом"
+                        hasHover={true}
+                        style={{
+                            width: '30%',         // Ширина для больших экранов
+                            height: '33vw',        // Адаптивная высота
+                            margin: '20px auto',
+                            borderRadius: '10px',
+                            overflow: 'hidden',
+                        }}
+                    />
+                </CardGrid>
+
+                <Footer style={{marginTop: 'auto'}}/>
+            </Group>
+            <style>{`
+                @media (max-width: 768px) {
+                    .vkuiCardGrid {
+                        margin-top: 10vw;  
+                    }
+
+                    .vkuiContentCard {
+                        width: 90%;        
+                        height: auto;     
+                    }
+
+                    .vkuiContentCard img {
+                        height: auto;      
+                        width: 100%;      
+                        object-fit: cover; 
+                    }
+                }
+                
+            `}</style>
         </Panel>
     );
 };
